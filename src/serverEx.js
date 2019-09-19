78a478f8ca63ed0001f6141fe7ac652af04a1dbf
mzeecob <reshimwe2025@gmail.com>
2019-09-19 15:54:03 +0200

    buffer and stream, and create read streams as well as write streams

---
+++ src/serverEx.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var fs=require('fs');
+
+
+var myReadStream = fs.createReadStream(__dirname + '/read.txt');
+var myWriteStream = fs.createWriteStream(__dirname + '/write.txt');
+
+myReadStream.on('data', function(chunk){
+    console.log("new chunk recived");
+    myWriteStream.write(chunk);
+});
+
+
+
+// // test our servers.
+// var server = http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('hey you');
+// });
+
+// server.listen(3000, '127.0.0.1');
+
+// console.log('listening to port 3000');
