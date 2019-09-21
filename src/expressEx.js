54bcf10db0b13dcd9b26bc640b62b387281c92ba
mzeecob <reshimwe2025@gmail.com>
2019-09-21 18:45:24 +0200

    access the parameter in the url like:id

--- src/expressEx.js
+++ src/expressEx.js
@@ -6,4 +6,9 @@ app.get('/', function(req, res){
     res.send('this is home page');
 });
 
+// access the parameter in url like id
+app.get('/profile/:id', function(req, res){
+    res.send("you have request the user with this id " + req.params.id);
+})
+
 app.listen(3000);
