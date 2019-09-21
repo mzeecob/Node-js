5101a7bf400b3fd97af91b2342314b431f9b298b
mzeecob <reshimwe2025@gmail.com>
2019-09-21 11:50:13 +0200

    routes file

---
+++ src/routesEX.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var fs=require('fs');
+
+var server = http.createServer(function (req, res) {
+    console.log('request was made: ' + req.url)
+    if(req.url === '/home' || req.url === '/'){
+
+        res.writeHead(200, {'content-type': 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+
+    }
+});
+
+server.listen(3000, '127.0.0.1');
+
+console.log('listening to port 3000');
