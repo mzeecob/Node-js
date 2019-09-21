67f0316aa9c2486fadfbd7a9c289626e9979b39e
mzeecob <reshimwe2025@gmail.com>
2019-09-21 11:37:46 +0200

    serving JSON data

--- src/serverEx.js
+++ src/serverEx.js
@@ -18,10 +18,20 @@ var fs=require('fs');
 
 //our servers.
 var server = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, {'Content-Type': 'application/json'});
 
-    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-    myReadStream.pipe(res);
+    // var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    // myReadStream.pipe(res);
+
+    // serving JSON Data
+    var myObj = {
+        name: "mzee",
+        job: "Programmer",
+        age: 30
+    }
+
+    res.end(JSON.stringify(myObj));
+
 });
 
 server.listen(3000, '127.0.0.1');
