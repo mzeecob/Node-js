bc3660a63c4064a4b8001612e25d97c0260d8020
mzeecob <reshimwe2025@gmail.com>
2019-09-21 11:18:25 +0200

    render html page in server

--- src/serverEx.js
+++ src/serverEx.js
@@ -16,15 +16,12 @@ var fs=require('fs');
 // myReadStream.pipe(myWriteStream);
 
 
-
 //our servers.
 var server = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.writeHead(200, {'Content-Type': 'text/html'});
 
-    var myReadStream = fs.createReadStream(__dirname + '/read.txt');
+    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
     myReadStream.pipe(res);
-
-
 });
 
 server.listen(3000, '127.0.0.1');
